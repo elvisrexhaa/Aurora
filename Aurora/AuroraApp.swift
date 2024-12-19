2e5eec6e4a6ba8cc52aeb99c154ac47a0543ba60
elvisrexhaa <elvis2000@hotmail.co.uk>
2024-12-19 12:42:15 +0000

    Initial Commit

---
+++ Aurora/AuroraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuroraApp.swift
+//  Aurora
+//
+//  Created by Elvis Rexha on 19/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct AuroraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuroraTests/AuroraTests.swift
@@ -0,0 +1,17 @@
+//
+//  AuroraTests.swift
+//  AuroraTests
+//
+//  Created by Elvis Rexha on 19/12/2024.
+//
+
+import Testing
+@testable import Aurora
+
+struct AuroraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
